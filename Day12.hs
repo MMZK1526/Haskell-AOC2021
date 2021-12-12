373220bd95562f10207d0533e712e9ae3b4f8dc1
Yitang Chen <yc4120@ic.ac.uk>
2021-12-12 06:40:55 +0000

    Refactor

--- Day12.hs
+++ Day12.hs
@@ -15,9 +15,9 @@ day12Part1 :: [(String, String)] -> Integer
 day12Part1 = go "start" . filter (("start" /=) . snd)
   where
     go "end" _     = 1
-    go start paths = sum $ map (\next -> go next (filterPath next)) nexts
+    go start paths = sum $ ap go filterPath <$> nexts
       where
-        nexts = map snd $ filter ((== start) . fst) paths
+        nexts = snd <$> filter ((== start) . fst) paths
         filterPath next
           | isUpper (head next) = paths
           | otherwise           = filter ((next /=) . snd) paths
@@ -28,13 +28,13 @@ day12Part2 = go True S.empty "start" . filter (("start" /=) . snd)
   where
     go _ _ "end" _ = 1
     go canGoBack set start paths
-      | isUpper (head start) = sum $ map (flip (go canGoBack set) paths) nexts
-      | start `notElem` set  = sum $ map (flip (go canGoBack set') paths) nexts
-      | canGoBack            = sum $ map (flip (go False set') paths) nexts
+      | isUpper (head start) = sum $ flip (go canGoBack set) paths <$> nexts
+      | start `notElem` set  = sum $ flip (go canGoBack set') paths <$> nexts
+      | canGoBack            = sum $ flip (go False set') paths <$> nexts
       | otherwise            = 0
       where
         set'  = S.insert start set
-        nexts = map snd $ filter ((== start) . fst) paths
+        nexts = snd <$> filter ((== start) . fst) paths
 
 main :: IO ()
 main = do
