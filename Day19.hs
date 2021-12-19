c166da0c182266a2932394345055b85d067476cb
Yitang Chen <yc4120@ic.ac.uk>
2021-12-19 16:04:43 +0000

    Refactor

--- Day19.hs
+++ Day19.hs
@@ -5,37 +5,44 @@ module Day19 where
 -- Question source: https://adventofcode.com/2021/day/19
 
 import           Control.Monad
-import           Data.Array (Array)
-import qualified Data.Array as A
 import           Data.Bifunctor
-import qualified Data.Foldable as F
+import           Data.List
 import           Data.Map (Map)
 import qualified Data.Map as M
 import           Data.Maybe
 import qualified Data.Set as S
 import qualified Data.Text as T
 import           Data.Tuple
-import qualified Gadgets.Array as A
 import qualified Gadgets.Counter as C
 import qualified Gadgets.Set as S
 import           Utilities
 
-type Point    = [Int]
--- type Rotation = Array Int Int
-type Rotation_ = Point -> Point
+type Rotation = [(Int, Int)] -- The second Int represents the sign
 
--- comp :: Rotation -> Rotation -> Rotation
--- comp r r' = id <$> r
+comp :: Rotation -> Rotation -> Rotation
+comp r r' = map (\i -> let (i', s) = r !! i in second (s *) $ r' !! i') [0..2]
+
+rot :: Rotation -> [Int] -> [Int]
+rot r p = map (\i -> let (i'', s) = r !! i in s * p !! i'') [0..2]
+
+rid :: Rotation
+rid = [(0, 1), (1, 1), (2, 1)]
+
+rotations :: [Rotation]
+rotations
+  = nub $ join (liftM2 comp)
+  $ concatMap (take 4 . flip iterate rid . comp) [ [(0, 1), (2, 1), (1, -1)]
+                                                 , [(2, -1), (1, 1), (0, 1)]
+                                                 , [(1, 1), (0, -1), (2, 1)] ]
 
 -- Finds the transformation and offset of each scanner.
-scannerPs :: [[Point]] -> Map Int (Rotation_, Point)
-scannerPs pz = findScannerPos (S.singleton 0) (M.singleton 0 (id, [0, 0, 0]))
+scannerPs :: [[[Int]]] -> Map Int (Rotation, [Int])
+scannerPs pz = findScannerPos (S.singleton 0) (M.singleton 0 (rid, [0, 0, 0]))
   where
     cmp ps ps'
       = maybeToList $ listToMaybe $ dropWhile ((< 12) . fst . snd)
-      $ fmap ( second (maximum . fmap swap . C.group . liftM2 (zipWith (-)) ps) 
-             . ap (,) (`map` ps')
-             ) rotations
+      $ map (second (maximum . map swap . C.group . liftM2 (zipWith (-)) ps))
+      $ ap (,) ((<$> ps') . rot) <$> rotations
     scannerRPos
       = do ((i, ps), (i', ps')) <- join (liftM2 (,)) $ zip [0..] pz
            guard $ i /= i'
@@ -50,28 +57,16 @@ scannerPs pz = findScannerPos (S.singleton 0) (M.singleton 0 (id, [0, 0, 0]))
           | f == i && M.notMember j k = go (S.insert j fs) k' ps
           | otherwise                 = go fs k ps
           where
-            k'         = M.insert j (tI . t, zipWith (+) posI (tI pos)) k
-            (tI, posI) = k M.! i
-    rotations = [\[x, y, z] -> [-x, y, -z], \[x, y, z] -> [-x, -y, z],
-                 \[x, y, z] -> [-x, z, y], \[x, y, z] -> [-x, -z, -y],
-                 \[x, y, z] -> [-y, x, z], \[x, y, z] -> [-y, -x, -z],
-                 \[x, y, z] -> [-y, z, -x], \[x, y, z] -> [-y, -z, x],
-                 \[x, y, z] -> [-z, y, x], \[x, y, z] -> [-z, -y, -x],
-                 \[x, y, z] -> [-z, x, -y], \[x, y, z] -> [-z, -x, y],
-                 \[x, y, z] -> [x, y, z], \[x, y, z] -> [x, -y, -z],
-                 \[x, y, z] -> [x, z, -y], \[x, y, z] -> [x, -z, y],
-                 \[x, y, z] -> [y, x, -z], \[x, y, z] -> [y, -x, z],
-                 \[x, y, z] -> [y, z, x], \[x, y, z] -> [y, -z, -x],
-                 \[x, y, z] -> [z, y, -x], \[x, y, z] -> [z, -y, x],
-                 \[x, y, z] -> [z, x, y], \[x, y, z] -> [z, -x, -y]]
+            k' = let (tI, posI) = k M.! i
+                 in  M.insert j (comp tI t, zipWith (+) posI (rot tI pos)) k
 
-day19Part1 :: Map Int (Rotation_, Point) ->[[Point]] -> Int
-day19Part1 scanners = length . S.fromList . concat . zipWith merge [0..]
+day19Part1 :: Map Int (Rotation, [Int]) -> [[[Int]]] -> Int
+day19Part1 scs = length . S.fromList . concat . zipWith merge [0..]
   where
-    merge i = let (t, oft) = scanners M.! i in map (flip (zipWith (+)) oft . t)
+    merge i = let (r, oft) = scs M.! i in map (flip (zipWith (+)) oft . rot r)
 
-day19Part2 ::  Map Int (Rotation_, Point) -> Int
-day19Part2 
+day19Part2 :: Map Int (Rotation, [Int]) -> Int
+day19Part2
   = maximum . join (liftM2 ((sum .) . zipWith (-))) . M.elems . M.map snd
 
 main :: IO ()
