d6a80f360b5533ca496660a840b7cfd61dbd1a5b
Yitang Chen <yc4120@ic.ac.uk>
2021-12-20 11:18:12 +0000

    Refactor

--- Day20.hs
+++ Day20.hs
@@ -7,7 +7,7 @@ module Day20 where
 
 import           Data.Array (Array)
 import qualified Data.Array as A
-import           Data.List
+import           Data.Bifunctor
 import           Data.Map (Map)
 import qualified Data.Map as M
 import           Data.Maybe
@@ -15,32 +15,32 @@ import qualified Data.Text as T
 import qualified Gadgets.Array as A
 import           Utilities
 
--- Does 2 transformation at once.
-trans2 :: Array Int Char -> Map (Int, Int) Char -> Map (Int, Int) Char
+-- Does 2 transformations at once.
+trans2 :: Array Int Int -> Map (Int, Int) Int -> Map (Int, Int) Int
 trans2 algo image = transform 2 $ transform 3 image
   where
     (rMin, cMin)      = fst $ fst $ fromJust $ M.minViewWithKey image
     (rMax, cMax)      = fst $ fst $ fromJust $ M.maxViewWithKey image
-    bin '#'           = '1'
-    bin '.'           = '0'
     transform i image = foldl go M.empty imgRange
       where
         imgRange   = A.range ((rMin - i, cMin - i), (rMax + i, cMax + i))
         go img pos = M.insert pos (algo A.! uncurry readAt pos) img
-        readAt r c = binToDec $ map (bin . fromMaybe '.' . (image M.!?)) 
-                   $ sort [(r + i, c + j) | i <- [-1..1], j <- [-1..1]]
+        readAt r c = foo $ map (fromMaybe 0 . (image M.!?)) 
+                   $ [(r + i, c + j) | i <- [-1..1], j <- [-1..1]]
+        foo = foldl ((. fromIntegral) . (+) . (2 *)) 0
 
-day20Part1 :: Array Int Char -> Map (Int, Int) Char -> Int
-day20Part1 = ((length . M.filter ('#' ==)) .) . trans2
+day20Part1 :: Array Int Int -> Map (Int, Int) Int -> Int
+day20Part1 = (sum .) . trans2
 
-day20Part2 :: Array Int Char -> Map (Int, Int) Char -> Int
-day20Part2 = ((length . M.filter ('#' ==)) .) 
-           . flip flip 25 . ((!!) .) . iterate . trans2
+day20Part2 :: Array Int Int -> Map (Int, Int) Int -> Int
+day20Part2 = (sum .) . flip flip 25 . ((!!) .) . iterate . trans2
 
 main :: IO ()
 main = do
   [algo', image'] <- fmap T.unpack . T.splitOn "\n\n" <$> readInput "day20"
-  let algo  = A.fromList algo'
-  let image = M.fromAscList $ A.assocs $ A.from2DListR $ lines image'
+  let bin c = if c == '#' then 1 else 0
+  let algo  = A.fromList $ bin <$> algo'
+  let image = M.fromAscList $ map (second bin) 
+            $ A.assocs $ A.from2DListR $ lines image'
   print $ day20Part1 algo image
   print $ day20Part2 algo image
